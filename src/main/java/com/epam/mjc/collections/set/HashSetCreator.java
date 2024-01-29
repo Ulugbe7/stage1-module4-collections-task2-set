cfab63eb117adcc6d8b00d45d11e4913549adaad
ulugbek <ulugbekyusupov272@gmail.com>
2024-01-29 15:36:23 +0500

    finish

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -1,9 +1,26 @@
 package com.epam.mjc.collections.set;
 
+import javax.swing.*;
 import java.util.HashSet;
 import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (Integer i : sourceList) {
+            if (i % 2 == 1) {
+                set.add(i);
+                set.add(2 * i);
+            } else {
+                int temp = i;
+                set.add(temp);
+                while (temp % 2 == 0) {
+                    temp = temp / 2;
+                    set.add(temp);
+                }
+            }
+        }
+        return set;
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,17 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        Set<Integer> set = new TreeSet<>();
+        for (Integer i : sourceList) {
+            int temp = i * i;
+            if (temp >= lowerBound && temp <= upperBound) {
+                set.add(temp);
+            }
+        }
+        return set;
     }
 }
